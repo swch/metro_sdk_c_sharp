9a5b963a94c87b1b220d0000c153c7d44569ed73
Mark Budos <markbudos@gmail.com>
2020-05-26 00:49:08 -0700

    Update AccountTests.cs

--- cardsavr_tests/AccountTests.cs
+++ cardsavr_tests/AccountTests.cs
@@ -16,9 +16,8 @@ namespace cardsavr_tests
             // this just verifies that nullable property values are null by default, without explicitly
             // setting them to null in the constructor (as expected).
             Account a = new Account();
-            Assert.Null(a.require_master_password);
-            Assert.Null(a.auto_login);
-            Assert.Null(a.show_payment_method);
+            Assert.Null(a.site_hostname);
+            Assert.Null(a.username);
         }
 
         [Fact]
@@ -27,15 +26,15 @@ namespace cardsavr_tests
             Account a = new Account()
             {
                 id = 12,
-                auto_login = true
+                site_hostname = "amazon.com"
             };
 
             // be sure the serialized string contains ONLY the non-null properties.
             string s = JsonConvert.SerializeObject(a);
             Assert.Contains("id", s);
-            Assert.Contains("auto_login", s);
+            Assert.Contains("amazon.com", s);
             Assert.DoesNotContain("show_payment_method", s);
-            Assert.DoesNotContain("last_login", s);
+            Assert.DoesNotContain("auto_login", s);
             Assert.DoesNotContain("created_on", s);
         }
 
@@ -46,9 +45,9 @@ namespace cardsavr_tests
             Account a = JsonConvert.DeserializeObject<Account>(s);
 
             // be sure the nullable values are null in the deerialized object.
-            Assert.Null(a.show_payment_method);
-            Assert.Null(a.auto_login);
-            Assert.Null(a.require_master_password);
+            Assert.Null(a.last_password_update);
+            Assert.Null(a.last_saved_card);
+            Assert.Null(a.last_updated_on);
         }
     }
 }
