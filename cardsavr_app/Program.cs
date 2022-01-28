25a23ab48d4cba9ec651809e44de1d353bbe0ccf
Mark Budos <markbudos@gmail.com>
2022-01-28 11:03:00 -0800

    Update Program.cs

--- cardsavr_app/Program.cs
+++ cardsavr_app/Program.cs
@@ -19,7 +19,7 @@ namespace cardsavr_app
                 ClientSession auth = await helper.LoginAndCreateSession("<USERNAME>", "<PASSWORD>", "{\"key\": \"my_trace\"}");
                 
                 PropertyBag cd = new PropertyBag(){{"my_fi", new PropertyBag(){{"token", "123"}}}};
-                ClientLogin login = await helper.CreateCard("testing_user", "default", 
+                ClientLogin login = await helper.CreateCard("testing_user", 
                     new Cardholder(){ email = "foo@foo.com", custom_data = cd, cuid = "12345" },
                     new Card(){ pan="4111111111111111", cvv="111", expiration_month="01", expiration_year="25" },
                     new Address(){ first_name="Strivve", last_name="User", is_primary=true, phone_number = "5555555555", address1="1234 1st ave", city="Seattle", subnational="WA", postal_code="98006", country="USA" }
